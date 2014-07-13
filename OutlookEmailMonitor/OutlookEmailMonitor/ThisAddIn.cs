3d9aef4d910392a606223fed6371b3afbaf93bb3
jacqt <anthony.guo@some.ox.ac.uk>
2014-07-13 00:52:09 -0700

    Added messaging parsing

--- OutlookEmailMonitor/OutlookEmailMonitor/ThisAddIn.cs
+++ OutlookEmailMonitor/OutlookEmailMonitor/ThisAddIn.cs
@@ -5,17 +5,102 @@ using System.Text;
 using System.Xml.Linq;
 using Outlook = Microsoft.Office.Interop.Outlook;
 using Office = Microsoft.Office.Core;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Resources;
 
 namespace OutlookEmailMonitor
 {
     public partial class ThisAddIn
     {
-        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        Outlook.Inspectors inspectors;
+        Outlook.NameSpace outlook_namespace;
+        Outlook.MAPIFolder inbox;
+        Outlook.Items items;
+        Storage storage;
+        int most_recent_mail_hashcode;
+        
+        private async void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
+            //inspectors = this.Application.Inspectors;
+            //inspectors.NewInspector += new Microsoft.Office.Interop.Outlook.InspectorsEvents_NewInspectorEventHandler(Inspectors_NewInspector);
+            //this.Application.NewMail += new Microsoft.Office.Interop.Outlook.MailItem;
+
+            outlook_namespace = this.Application.GetNamespace("MAPI");
+            inbox = outlook_namespace.GetDefaultFolder(
+                Outlook.OlDefaultFolders.olFolderInbox);
+
+            items = inbox.Items;
+            items.ItemAdd +=
+                new Outlook.ItemsEvents_ItemAddEventHandler(items_ItemAdd);
+
+            await ProcessUnprocessedEmails(); 
+            Debug.WriteLine("Hello debugger - this is the application speaking! We finished processing the unprocessed emails");
+        }
+
+        private async Task ProcessUnprocessedEmails(){
+            object item = items.GetNext();
+            int i = 0;
+            while (isUnprocessed(item) && i != 30000)
+            {
+                Outlook.MailItem mail_item = item as Outlook.MailItem;
+                if (mail_item != null)
+                {
+                    DateTime received_time  = mail_item.ReceivedTime;
+                    DateTime sent_time      = mail_item.SentOn;
+                    TimeSpan latency        = received_time.Subtract(sent_time);
+                    //Debug.WriteLine(received_time.ToString());
+                    //Debug.WriteLine(sent_time.ToString());
+                    Debug.WriteLine("Latency: " + latency.ToString());
+                    //Debug.WriteLine(mail_item);
+                }
+                item = items.GetNext();
+                ++i;
+            }
+        }
+
+        private bool isUnprocessed(object item)
+        {
+            Outlook.MailItem mail_item = item as Outlook.MailItem;
+            if (mail_item != null)
+            {
+                int hash = mail_item.GetHashCode();
+            }
+            return false;
+            
         }
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
         {
+            //TODO
+        }
+
+        void items_ItemAdd(object item)
+        {
+            Outlook.MailItem mail_item = item as Outlook.MailItem;
+            if (mail_item != null)
+            {
+                DateTime received_time  = mail_item.ReceivedTime;
+                DateTime sent_time      = mail_item.SentOn;
+                TimeSpan latency        = received_time.Subtract(sent_time);
+                Debug.WriteLine(received_time.ToString());
+                Debug.WriteLine(sent_time.ToString());
+                Debug.WriteLine(latency.ToString());
+                Debug.WriteLine(mail_item);
+            }
+        }
+        void Inspectors_NewInspector(Microsoft.Office.Interop.Outlook.Inspector Inspector)
+        {
+            Outlook.MailItem mailItem = Inspector.CurrentItem as Outlook.MailItem;
+            if (mailItem != null)
+            {
+                if (mailItem.EntryID == null)
+                {
+                    //mailItem.Subject = "This text was added by using code";
+                    //mailItem.Body = "This text was added by using code";
+                }
+
+            }
         }
 
         #region VSTO generated code
---
+++ OutlookEmailMonitor/OutlookEmailMonitor/storage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutlookEmailMonitor
+{
+    class Storage
+    {
+        public void saveFile(String name, String data)
+        {
+
+        }
+
+        public String loadFile(String name)
+        {
+            return "TODO";
+        }
+
+    }
+}
